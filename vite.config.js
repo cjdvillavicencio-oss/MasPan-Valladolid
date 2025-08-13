d71f7605f0e8f67991ffc95bc46eb8fd09e5f60d
cjdvillavicencio-oss <cjdvillavicencio@gmail.com>
2025-08-14 01:13:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ pon aquí el NOMBRE EXACTO de tu repo:
-const repo = '/MasPan-Valladolid/'
-
 export default defineConfig({
-  base: '/MasPan-Valladolid/', // ← tu repo
   plugins: [react()],
 })
 
